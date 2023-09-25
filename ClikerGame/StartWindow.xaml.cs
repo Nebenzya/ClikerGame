bbba7fad1b85e25fd9577f2d6306f1b95cd6ca4e
iazin <107532410+iazin@users.noreply.github.com>
2023-09-25 22:08:49 +0500

    Add files via upload
    Добавлена стартовая страница

---
+++ ClikerGame/StartWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClikerGame
+{
+    /// <summary>
+    /// Логика взаимодействия для StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            //Тут будет запрос к БД
+            MainWindow mainW = new MainWindow();
+            mainW.Show();
+        }
+
+        private void RatingButton_Click(object sender, RoutedEventArgs e)
+        {
+            RatingWindow raitingW = new RatingWindow();
+            raitingW.Show();
+        }
+    }
+}
