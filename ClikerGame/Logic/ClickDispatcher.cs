791eb062178c0176cdfa1976580e2b2b201bd4c1
uhbu3ok <ShiryaevAS98@yandex.ru>
2023-09-27 22:04:09 +0300

    Подсчет времени игры

--- ClikerGame/Logic/ClickDispatcher.cs
+++ ClikerGame/Logic/ClickDispatcher.cs
@@ -11,20 +11,24 @@ namespace ClikerGame.Logic
     {
         public long click = 1;//кол-во за нажатие
         public long clickСounter = 0;//счетчик
-        public int priceModification = 25;//цена покупки
+        public int[] priceModification = { 25, 25 };//цена покупки
 
         public void ClickСookie() //счетчик кликов
         {
             clickСounter += click;
         }
-        public void ModificationDoubleСlick() //умножение клика х2
+        public void ModificationDoubleСlick() //умножение клика х2, [0]
         {
-            if (clickСounter >= priceModification)
+            if (clickСounter >= priceModification[0])
             {
                 click *= 2;
-                clickСounter -= priceModification;
-                priceModification += priceModification;// подорожание покупки х2 клика, можно придумать формулу посложнее
+                clickСounter -= priceModification[0];
+                priceModification[0] += priceModification[0];// подорожание покупки х2 клика, можно придумать формулу посложнее
             }
+        }
+        public void AutoClick()//авто клик каждое n-время, [1]
+        {
+
         }
     }
 }
